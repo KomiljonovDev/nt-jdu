c15af49f2e71840238e1674234ac0e0b9545fbcf
Komiljonov O <uzokdeveloper@gmail.com>
2025-02-21 20:02:19 +0500

    MINOR: Add all Models and migrations, API sanctum, Complete Subject CRUD

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\SubjectController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::prefix('subjects')->group(function () {
+        Route::get('/', [SubjectController::class, 'index']);
+        Route::get('/{subject}', [SubjectController::class, 'show']);
+        Route::put('/{subject}', [SubjectController::class, 'update']);
+        Route::delete('/{subject}', [SubjectController::class, 'delete']);
+    });
+});
